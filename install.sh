ac4c0178a2b41b032e8ba541e68fd6a90a59e1eb
Thomas Enns <thomas.p.a.enns@gmail.com>
2024-01-16 16:45:00 -0500

    readme and installer update

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-pip install pandas
-pip install gurobipy>=9.5.1
-pip install networkx
-pip install dash
+python3.10 -m pip install pandas
+python3.10 -m pip install gurobipy>=9.5.1
+python3.10 -m pip install networkx
+python3.10 -m pip install dash
+echo "Setup sequence completed!"
