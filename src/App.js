e6912dd8083d9c094e3d045632882c9f04a47415
Natalie Tam <30819550+nataliet57@users.noreply.github.com>
2024-03-01 21:47:03 -0500

    Created landing page and /map route

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Routes,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Map from "./routes/Map";
+// import Work from "./Components/Work";
+// import Testimonial from "./Components/Testimonial";
+// import Contact from "./Components/Contact";
+// import Footer from "./Components/Footer";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/map" element={<Map />} />
+        </Routes>
+      </BrowserRouter>
+      {/* <About />
+      <Work />
+      <Testimonial />
+      <Contact />
+      <Footer /> */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes/Map.js
@@ -0,0 +1,17 @@
+import Iframe from 'react-iframe'
+import React from 'react'
+
+const Map = () => {
+  return (
+    <>
+      <h2>TESTTINNNNGG</h2>
+      <Iframe url = 'http://127.0.0.1:8050/'
+              width="100%"
+              height="1000px"
+      />
+
+    </>
+  )
+}
+
+export default Map
